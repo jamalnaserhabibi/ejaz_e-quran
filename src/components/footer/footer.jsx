9f374e41af95ecf6bbed31ecd6c2dd5a38f09945
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2025-01-10 08:10:41 +0500

    .

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -11,9 +11,74 @@ import {
 } from "mdb-react-ui-kit";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Link } from 'react-router-dom';
 
 AOS.init();
 export default function App() {
+    const books = 
+    [
+      { 
+        id: 1, 
+        title: "عقیده طحاوی",
+        // bimage: aqida,
+        write: "محمد بن حسن طحاوی",
+        publish: "ناشر",
+        year: "سال انتشار",
+        size: "سایز کتاب",
+        content: " محتویات در مورد کتاب عقیده طحاوی محتویات در مورد کتاب عقیده طحاوی محتویات در مورد کتاب عقیده طحاوی محتویات در مورد کتاب عقیده طحاوی",
+        numberofvol:1,
+        vol:1,
+      },
+      { 
+        id: 2, 
+        title: "هزار حدیث",
+        // bimage: hazarhadith,
+        write: "...",
+        publish: "احمد",
+        year: "1401",
+        size: "2.3 MB",
+        content: " محتویات در مورد کتاب هزار حدیث محتویات در مورد کتاب هزار حدیث محتویات در مورد کتاب هزار حدیث محتویات در مورد کتاب هزار حدیث",
+        numberofvol:1,
+        vol:1,
+      },
+      { 
+        id: 3, 
+        title: "دلایل خیرات",
+        // bimage: dalayel,
+        write: "...",
+        publish: "احمد",
+        year: "1401",
+        size: "1.3 MB",
+        content: " محتویات در مورد کتاب دلایل خیرات محتویات در مورد کتاب دلایل خیرات محتویات در مورد کتاب دلایل خیرات محتویات در مورد کتاب دلایل خیرات",
+        numberofvol:1,
+        vol:1,
+      },
+      { 
+        id: 4, 
+        title: "تفسیر کابلی",
+        // bimage: tafsir,
+        write: "...",
+        publish: "احمد",
+        year: "1401",
+        size: "4.3 MB",
+        content: " محتویات در مورد کتابتفسیر کابلی محتویات در مورد کتابتفسیر کابلی محتویات در مورد کتابتفسیر کابلی محتویات در مورد کتاب تفسیر کابلی",
+        numberofvol:3,
+        vol:1,
+      },
+      { 
+        id: 5, 
+        title: "زیور بهشتی",
+        // bimage: ziwar,
+        write: "...",
+        publish: "احمد",
+        year: "1401",
+        size: "5.3 MB",
+        content: " محتویات در مورد کتاب  زیور بهشتی محتویات در مورد کتاب زیور بهشتی محتویات در مورد کتاب زیور بهشتی محتویات در مورد کتاب  زیور بهشتی",
+        numberofvol:4,
+        vol:1,
+      },
+    ];
+  
   return (
     <MDBFooter  className="footerClass text-center">
       <MDBContainer className="p-4">
@@ -99,11 +164,9 @@ export default function App() {
 
         <section data-aos="fade-up" className="mb-4">
           <p>
-            هدف ما حل معما های قرآنی تفسیر قرآن بالقرآن به زبان های دری انگلیسی
-            پشتو و عربی میباشد که به منظور هدایت امت و هدف ما حل معما های قرآنی
-            تفسیر قرآن بالقرآن به زبان های دری انگلیسی پشتو و عربی میباشد که به
-            منظور هدایت امت و هدف ما حل معما های قرآنی تفسیر قرآن بالقرآن به
-            زبان های دری انگلیسی پشتو و عربی میباشد که به منظور هدایت امت و
+          سایت اعجاز القران با راه اندازی مباحث علمی نایاب و بی پیشنه در پرتو تفسير قرآن با قرآن و تفسیر قرآن با حدیث در پی اصلاح عقائد و اعمال، توحید امت و بالا بردن سطح دانش نوع انسان بخصوص امت مسلمه در قرآن و اسلام است. 
+از ویژگی های سایت اعجاز القران است که مسائل را بطور واقع بینانه، بدون تبعیض و با تحقیق و تدقیق بیان می‌کند. 
+وب سایت اعجاز القرآن به اضافه به بیان عبارات قرآنی و توضیح عجائ .ب ان، می‌کوشد تا پرده از رموز، اسرار و علوم لدنی ایات، کلمات و حروف آن بردارد.
           </p>
         </section>
 
@@ -135,32 +198,21 @@ export default function App() {
             </div>
             </MDBCol>
             <MDBCol lg="3" md="6" sm="6" className="mb-4 mb-md-0">
+
             <div className="second">
-
-
-              <h5 className="text-uppercase">کتاب ها</h5>
-
-              <p>
-                <a href="#!" className="text-white">
-                  کتاب اول
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-white">
-                  کتاب دوم
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-white">
-                  کتاب سوم
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-white">
-                  کتاب چهارم
-                </a>
-              </p>
-            </div>
+  <h5 className="text-uppercase">کتاب ها</h5>
+  {books.slice(0, 4).map((book) => (
+    <p key={book.id}>
+      <a
+        href={`/bookdownload?bookid=${book.id}&booktitle=${book.title}&write=${book.write}&publish=${book.publish}&year=${book.year}&size=${book.size}&content=${book.content}&numberofvol=${book.numberofvol}&vol=${book.vol}`}
+        className="text-white"
+      >
+        {book.title}
+      </a>
+    </p>
+  ))}
+</div>
+            
             </MDBCol>
             <MDBCol lg="3" md="6" sm="6" className="mb-4 mb-md-0">
            <div className="third">
@@ -168,7 +220,9 @@ export default function App() {
               <h5 className="text-uppercase">موضوعات</h5>
 
                 <p>
-                  <a className="text-white">تفسیر قرآن</a>
+                 <Link style={{textDecoration:'none', color:'white'}} to="to=/taqrirList?identifier=tafsirQuranBelQuran">
+                 تفسیرالقرآن بالقرآن والحدیث
+                 </Link>
                 </p>
                 <p>
                   <a className="text-white">حل معمای قرآن</a>
@@ -187,7 +241,7 @@ export default function App() {
            <h5 className="text-uppercase">ارتباطات</h5>
               <p>
                 <MDBIcon icon="home" className="me-3" />
-               {" "} کابل افغانستان 
+               {" "} ناحیه هفتم شهر کابل افغانستان 
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
