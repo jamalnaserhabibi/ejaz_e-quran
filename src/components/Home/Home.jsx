14119ce4088aa674cd7eeca393564d10105a5329
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2026-02-06 19:36:53 +0430

    hero age enlightening

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,19 +3,19 @@ import bg from "../../assets/c.jpg";
 // import besm from "../../assets/besmgreen.png";
 import line from "../../assets/linegreen.png";
 import Carousel from "react-bootstrap/Carousel";
-import ToTop from '../toTop/toTop'
-import { useState ,useEffect} from "react";
+import ToTop from "../toTop/toTop";
+import { useState, useEffect } from "react";
 export default function Home() {
   const [slider, setSlider] = useState([]);
 
   useEffect(() => {
     const fetchSliders = async () => {
       try {
-        const response = await fetch('https://ejazquran.space/api/v1/sliders');
-        if (!response.ok) throw new Error('Failed to fetch documents');
+        const response = await fetch("https://ejazquran.space/api/v1/sliders");
+        if (!response.ok) throw new Error("Failed to fetch documents");
         const data = await response.json();
         setSlider(data.data);
-        
+
         // setFilteredDocs(data);
       } catch (err) {
         // setError(err.message);
@@ -30,20 +30,26 @@ export default function Home() {
     <>
       <div className="mainContainer">
         <div
-          style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0,0.8),rgba(0, 0, 0,0.4), rgba(0, 0, 0,0.2)), url(${bg})`,
-          }}
+         style={{
+  backgroundImage: `
+    linear-gradient(
+      to bottom,
+      rgba(0,0,0,0.35),
+      rgba(0,0,0,0.15),
+      rgba(0,0,0,0.05)
+    ),
+    url(${bg})
+  `
+}}
           className="main"
         >
           <div className="homeText">
             <div className="mainText">
               <h1 className="hometitle">اعجاز قرآن کریم</h1>
             </div>
-            <div className="mainText0">
-              {/* <img src={line} alt="" /> */}
-            </div>
+            <div className="mainText0">{/* <img src={line} alt="" /> */}</div>
             <div className="DesText">
-              <Carousel className="Homeslider"  interval={10000} slide={true}>
+              <Carousel className="Homeslider" interval={10000} slide={true}>
                 {slider.map((ayat, index) => (
                   <Carousel.Item key={index}>
                     <h2 className="ayat">{ayat.title}</h2>
@@ -63,7 +69,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <ToTop/>
+      <ToTop />
     </>
   );
 }
