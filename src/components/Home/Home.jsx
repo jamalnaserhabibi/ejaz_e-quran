81d06c4a7f174fb6b3f7ab85a1ec52ccc2e93211
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2026-02-09 15:12:37 +0430

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import "./Home.css";
-import bg from "../../assets/c.jpg";
+import bg from "../../assets/background.jpg";
 // import besm from "../../assets/besmgreen.png";
 import line from "../../assets/linegreen.png";
 import Carousel from "react-bootstrap/Carousel";
